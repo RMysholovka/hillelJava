7aa2b60f2837a3cf983df91f0aa42f0c774ea9b5
RMysholovka <rmysholovka@gmail.com>
2015-09-17 21:19:54 +0300

    Hello World commit

---
+++ Hillel/src/MyFirstClass.java
@@ -0,0 +1,26 @@
+/**
+ * Created by RMysholovka on 17.09.2015.
+ */
+public class MyFirstClass {
+    public static void main(String[] args) {
+        String helloMessage = "Hello world";
+        System.out.println(helloMessage);
+
+        final int romaBirthYear = 1980;
+
+        int romaAge = 35;
+        int alexAge = 33;
+
+        System.out.println("Before changes:" + romaBirthYear);
+
+        romaBirthYear = 2015;
+
+        System.out.println("I'm " + romaAge);
+
+        System.out.println("31" + 31);
+
+        System.out.println(31 + 31);
+
+
+    }
+}
