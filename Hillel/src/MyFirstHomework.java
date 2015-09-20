872586dea3db58d9f1050bff12a2ea4c453d1512
RomanMysholovka <rmysholovka@gmail.com>
2015-09-20 18:56:42 +0300

    Hello World commit

---
+++ Hillel/src/MyFirstHomework.java
@@ -0,0 +1,15 @@
+/**
+ * Created by user on 20.09.2015.
+ */
+public class MyFirstHomework {
+    public static void main(String[] args)
+
+    {
+
+        int carAge = 7;
+        System.out.println("My car is " + carAge + " ears");
+
+    }
+
+
+}
