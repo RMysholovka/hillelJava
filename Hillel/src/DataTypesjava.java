3a661367d24a32cb1edf0ae6bc3bffc3c462f6aa
RMysholovka <rmysholovka@gmail.com>
2015-09-21 21:05:13 +0300

    Hello World commit

---
+++ Hillel/src/DataTypesjava.java
@@ -0,0 +1,31 @@
+/**
+ * Created by RMysholovka on 21.09.2015.
+ */
+public class DataTypesjava {
+    public static void main(String[] args) {
+        int age = 35;
+
+        int days = age * 365;
+
+        int hours = days * 24;
+
+        int seconds = hours * 3600;
+
+        long milliseconds = seconds * 1000L;
+
+        long longseconds = milliseconds / 1000;
+
+        seconds = (int) longseconds;
+
+        seconds = (int) (milliseconds / 1000);
+
+        System.out.println(milliseconds);
+
+        double d = 3.54;
+        int i = 10;
+        long l = 1000000000000000000L;
+        float f = 2.5F;
+
+
+    }
+}
