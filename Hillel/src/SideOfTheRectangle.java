4afe4cb862d1ee9a3e2c5dcd62112284a6ce0632
RomanMysholovka <rmysholovka@gmail.com>
2015-09-27 23:03:19 +0300

    Hello World commit

--- Hillel/src/SideOfTheRectangle.java
+++ Hillel/src/SideOfTheRectangle.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
  */
 public class SideOfTheRectangle {
     public static void main(String[] args) {
-        Scanner vvod = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Please enter area of rectangle ");
-        int area = vvod.nextInt();
+        double area = scanner.nextDouble();
         System.out.print("Please enter perimetr of rectangle ");
-        int perimetr = vvod.nextInt();
+        double perimetr = scanner.nextDouble();
 
 
-        int b = (int) ((Math.sqrt(Math.pow(perimetr, 2) - 16 * area) + perimetr) / 4);
+        double b = ((Math.sqrt(Math.pow(perimetr, 2) - 16 * area) + perimetr) / 4);
 
-        int a = area / b;
+        double a = area / b;
 
         if (2 * a + 2 * b == perimetr) {
-            System.out.println("Side a of the rectangle = " + a +" and " + "Side b of the rectangle = " + b);
+            System.out.println("Side a of the rectangle = " + a + " and " + "Side b of the rectangle = " + b);
 
         } else
             System.out.println("The rectangle with such parameters does not exist");
