ee5303605ea556b4e58648460dd8fcab7f8a7ed0
RomanMysholovka <rmysholovka@gmail.com>
2015-09-29 23:13:59 +0300

    Hello World commit

---
+++ Hillel/src/Calculator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * Created by ${Roman} on 29.09.2015.
+ */
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Pleas enter number: ");
+        double number = scanner.nextDouble();
+        System.out.println("Pleas enter sign: ");
+        byte sign = scanner.nextByte();
+        System.out.println("Pleas enter second number: ");
+        double secondNumber = scanner.nextDouble();
+
+
+    }
+}
