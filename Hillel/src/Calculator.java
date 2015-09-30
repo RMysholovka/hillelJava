5a6964046a4fb2e085330f9ddc382aeb6c937fbc
RomanMysholovka <rmysholovka@gmail.com>
2015-10-01 01:22:09 +0300

    Hello World commit

--- Hillel/src/Calculator.java
+++ Hillel/src/Calculator.java
@@ -5,14 +5,70 @@ import java.util.Scanner;
  */
 public class Calculator {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Pleas enter number: ");
-        double number = scanner.nextDouble();
-        System.out.println("Pleas enter sign: ");
-        byte sign = scanner.nextByte();
-        System.out.println("Pleas enter second number: ");
-        double secondNumber = scanner.nextDouble();
+        System.out.println("Pleas enter number1: ");
+        int number1 = scanner.nextInt();
+        System.out.println("Pleas enter operation: ");
+        String operation = scanner.next();
+        System.out.println("Pleas enter second number2: ");
+        int number2 = scanner.nextInt();
+
+
+        int res = 0;
+        res = metodCalc(number1, operation, number2, res);
+        System.out.println("Result: " + res);
+
+
+        System.out.println("Pleas enter number3: ");
+        double number3 = scanner.nextDouble();
+        System.out.println("Pleas enter operation trunk or fract: ");
+        operation = scanner.next();
+        scanner.close();
+
+        double res1 = 0;
+        res1 = metFractTrunk(number3, res1, operation);
+        System.out.println("Result: " + res1);
 
 
     }
+
+    private static int metodCalc(int number1, String operation, int number2, int res) {
+        switch (operation) {
+            case "+":
+                res = number1 + number2;
+                break;
+            case "-":
+                res = number1 - number2;
+                break;
+            case "*":
+                res = number1 * number2;
+                break;
+            case ":":
+                res = number1 / number2;
+                break;
+        }
+        return res;
+    }
+
+    private static double metFractTrunk( double number3, double res1, String operation) {
+
+        switch (operation) {
+
+            case "trunk":
+                res1 = (int) number3;
+                break;
+            case "fract":
+                res1 = (double) (number3 - (int) number3);
+                break;
+        }
+        return res1;
+    }
+
 }
+
+
+
+
+
+
