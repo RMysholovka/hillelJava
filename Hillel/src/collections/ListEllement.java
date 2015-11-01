7b4b48b38ca0a64141fcd6b8a350a584f0d1e6e3
RomanMysholovka <rmysholovka@gmail.com>
2015-11-01 18:46:39 +0200

    Hello World commit (cherry picked from commit 8786410)

--- Hillel/src/collections/ListEllement.java
+++ Hillel/src/collections/ListEllement.java
@@ -4,8 +4,8 @@ package collections;
  * Created by ${Roman} on 28.10.2015.
  */
 public class ListEllement {
-    Object value;
-    ListEllement next;
+    public Object value;
+    public ListEllement next;
 
     public ListEllement(Object value) {
         this.value = value;
@@ -14,11 +14,10 @@ public class ListEllement {
     public int size() {
         if (next == null) {
             return 1;
-
         } else {
             return 1 + next.size();
         }
-        //  return next == null:! 1,1+next.size()
+        // return next == null ? 1 : 1 + next.size();
     }
 
     public void add(Object value) {
