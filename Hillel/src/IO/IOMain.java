2c53ca233d79a0e606d2f36989e7ada1f7b9de90
RMysholovka <rmysholovka@gmail.com>
2015-11-19 19:10:20 +0200

    Hello World commit

---
+++ Hillel/src/IO/IOMain.java
@@ -0,0 +1,10 @@
+package IO;
+
+/**
+ * Created by RMysholovka on 19.11.2015.
+ */
+public class IOMain {
+    public static void main(String[] args) {
+        System.out.println("yes\no");
+    }
+}
