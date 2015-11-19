c89823bd12fd00105833726593f1d304ac6c8394
RomanMysholovka <rmysholovka@gmail.com>
2015-11-19 11:36:48 +0200

    Hello World commit                                    (cherry picked from commit 8786410)

--- Hillel/src/pattern/decorator/Robusta.java
+++ Hillel/src/pattern/decorator/Robusta.java
@@ -1,19 +1,11 @@
 package pattern.decorator;
 
 /**
-<<<<<<< HEAD
- * Created by MFarsikov on 12.11.2015.
- */
-public class Robusta extends Beverage {
-    public Robusta() {
-        setCost(8);
-        setDescription("Robusta coffee");
-=======
  * Created by RMysholovka on 12.11.2015.
  */
-public class Robusta extends Beverage {
-    public Robusta() {
-        super(8, "Robusta coffe");
->>>>>>> origin/master
+        public class Robusta extends Beverage {
+            public Robusta() {
+                setCost(8);
+                setDescription("Robusta coffee");
     }
 }
