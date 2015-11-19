264c146397693a2ed3078fd82001db5e6a04c542
RMysholovka <rmysholovka@gmail.com>
2015-11-19 21:15:53 +0200

    Hello World commit

--- Hillel/src/IO/IOMain.java
+++ Hillel/src/IO/IOMain.java
@@ -1,10 +1,8 @@
 package IO;
 
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -14,16 +12,80 @@ import java.nio.file.Paths;
  */
 public class IOMain {
     public static void main(String[] args) {
+        try (DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream("d:/myData.dat"))) {
+            long[] longs = new long[]{1, 2, 3, 4, 5, 6};
+            for (long aLong : longs) {
+                dataOutputStream.writeLong(aLong);
+            }
 
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        try (DataInputStream dataInputStream = new DataInputStream(new FileInputStream("d:/myData.dat"))) {
+            try {
+                while (true)
+                    System.out.println(dataInputStream.readLong());
+            } catch (EOFException e) {
+                System.out.println("end");
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+
+    }
+
+
+    private static void bufferedRead() {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream("d:/myFile.txt"), Charset.forName("windows-1251")))) {
+            String value = null;
+
+            while ((value = bufferedReader.readLine()) != null) {
+                System.out.println(value);
+            }
+            //throw new IOException("file bla bla bla");
+
+        } catch (IOException e) {
+            System.out.println(e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void charRead() {
+        try (InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream("d:/myFile.txt"), Charset.forName("windows-1251"))) {
+
+            int value;
+            while ((value = inputStreamReader.read()) != -1) {
+                System.out.print((char) value);
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    private static void copyBytes() {
+        try (InputStream inputStream = new FileInputStream("d:/myFile.txt");
+             OutputStream outputStream = new FileOutputStream("d:/copyOfMyFile.txt")
+        ) {
+            int value;
+            while ((value = inputStream.read()) != -1) {
+                outputStream.write(value);
+            }
+        } catch (IOException e) {
+            System.out.print(e);
+        }
+    }
+
+    private static void readWithResources() {
         try (InputStream inputStream = new FileInputStream("d:/myFile.txt")) {
 
 
             int value;
             while ((value = inputStream.read()) != -1) {
-                System.out.println((char) value);
+                System.out.print((char) value);
             }
         } catch (IOException e) {
-            System.out.println(e);
+            System.out.print(e);
         }
     }
 
@@ -34,13 +96,13 @@ public class IOMain {
             inputStream = new FileInputStream("d:/myFile.txt");
             int value;
             while ((value = inputStream.read()) != -1) {
-                System.out.println((char) value);
+                System.out.print((char) value);
 
             }
 
 
         } catch (IOException e) {
-            System.out.println(e);
+            System.out.print(e);
 
         } finally {
 
@@ -48,7 +110,7 @@ public class IOMain {
                 try {
                     inputStream.close();
                 } catch (IOException e) {
-                    System.out.println(e);
+                    System.out.print(e);
                 }
             }
 
