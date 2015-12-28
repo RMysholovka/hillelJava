9afc112bbab1f45e8a8a8863e6426915ac0913d9
RMysholovka <rmysholovka@gmail.com>
2015-12-28 20:11:11 +0200

    Hello World commit

--- Hillel/src/database/SingletonExample.java
+++ Hillel/src/database/SingletonExample.java
@@ -1,5 +1,7 @@
 package database;
 
+import java.util.ArrayList;
+
 /**
  * Created by RMysholovka on 21.12.2015.
  */
@@ -11,14 +13,61 @@ public class SingletonExample {
 
     }
 
-    public static synchronized SingletonExample getInstance() {
+    public static SingletonExample getInstance() {
         if (instance == null) {
-            if (instance instanceof Object) {
-                instance = new SingletonExample();
-
+            synchronized (SingletonExample.class) {
+                if (instance == null) {
+                    instance = new SingletonExample();
+                }
             }
         }
         return instance;
     }
 
+    public static void clear() {
+        instance = null;
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        ArrayList<SingletonExample> singletonExamples = new ArrayList<>();
+        singletonExamples.add(null);
+        singletonExamples.add(null);
+
+        int i = 0;
+        while (true) {
+            i++;
+
+
+            Thread thread1 = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    SingletonExample singletonExample = SingletonExample.getInstance();
+                    singletonExamples.set(0, singletonExample);
+                }
+            });
+
+
+            Thread thread2 = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    SingletonExample singletonExample = SingletonExample.getInstance();
+
+                    singletonExamples.set(1, singletonExample);
+                }
+            });
+
+            thread1.start();
+            thread2.start();
+
+            thread1.join();
+            thread2.join();
+
+            if (singletonExamples.get(0) != singletonExamples.get(1)) {
+                break;
+            }
+            clear();
+
+        }
+    }
 }
