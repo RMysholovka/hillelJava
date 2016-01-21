736c4ec6b6f3c4aa0505404870bfae84d63e5647
RomanMysholovka <rmysholovka@gmail.com>
2016-01-21 21:01:24 +0200

    NEw year lessons 2016

--- Hillel/src/java8features/Java8Main.java
+++ Hillel/src/java8features/Java8Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.function.*;
 import java.util.stream.Collectors;
 
 /**
@@ -12,6 +12,42 @@ import java.util.stream.Collectors;
  */
 public class Java8Main {
     public static void main(String[] args) {
+        streamExample();
+
+    }
+
+    private static void functionExample() {
+        List<Apple> apples = createApples(10, (Supplier<Apple>) Apple::new);
+
+        consumeApples(apples, new Consumer<Apple>() {
+            @Override
+            public void accept(Apple apple) {
+                System.out.println(apple);
+            }
+        });
+
+
+        consumeApples(apples, System.out::print);
+
+        System.out.println(mapToString(apples, Apple::getColor));
+    }
+
+    public static List<String> mapToString(List<Apple> apples, Function<Apple, String> mapper) {
+        List<String> strings = new ArrayList<>();
+        for (Apple apple : apples) {
+            strings.add(mapper.apply(apple));
+        }
+        return strings;
+    }
+
+    public static void consumeApples(List<Apple> apples, Consumer<Apple> consumer) {
+        for (Apple apple : apples) {
+            consumer.accept(apple);
+        }
+    }
+
+
+    private static void streamExample() {
         List<Apple> apples = new ArrayList<>();
         apples.add(new Apple("Red", 150));
         apples.add(new Apple("Green", 180));
@@ -32,12 +68,40 @@ public class Java8Main {
 
         heavyApples = filter(apples, apple -> apple.getWeight() > 170);
 
+        Predicate<Apple> isHeavy = Java8Main::isHeavy;
+
+        Predicate<Apple> isGreen = apple -> apple.getColor().equals("Green");
+
+        Predicate<Apple> isHeavyAndGreen = isGreen.and(isHeavy);
+
         heavyApples = apples.stream()
-                .filter(Java8Main::isHeavy)
-                .filter(apple -> apple.getColor().equals("Green"))
+                .filter(isHeavyAndGreen)
                 .collect(Collectors.toList());
 
-        System.out.println(heavyApples);
+
+        List<String> colors = apples.stream()
+                .map(Apple::getColor)
+                .collect(Collectors.toList());
+        System.out.println(colors);
+
+
+    }
+
+
+    public static List<Apple> createApples(int count, Supplier<Apple> appleSupplier) {
+        List<Apple> apples = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            apples.add(appleSupplier.get());
+        }
+        return apples;
+    }
+
+    public static List<Apple> createApples(int count, BiFunction<String, Integer, Apple> appleSupplier) {
+        List<Apple> apples = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            apples.add(appleSupplier.apply("Red", 50));
+        }
+        return apples;
     }
 
     private static boolean isHeavy(Apple apple) {
